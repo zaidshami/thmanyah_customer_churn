13b8555c9091e7f3fe13857b7bbf7b78e5004334
zaidshami <zaidalshami8@gmail.com>
2025-08-02 02:49:09 +0300

    install xgboost inside eval script

--- sagemaker_pipeline/evaluate.py
+++ sagemaker_pipeline/evaluate.py
@@ -3,7 +3,7 @@
 import subprocess
 # subprocess.check_call(["pip", "install", "xgboost", "scikit-learn", "--quiet"])
 subprocess.check_call(["pip", "install", "--quiet", "xgboost==1.7.6", "pandas==1.5.3",
-                       "scikit-learn==1.2.2", "numpy==1.23.5","--force-reinstall"])
+                       "scikit-learn==1.2.2", "numpy==1.23.5"])
 
 import argparse
 import os
